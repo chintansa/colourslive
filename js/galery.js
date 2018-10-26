6d66cac3eb851d73824eca08274ebc6735a0efe2
Chirag chopra <iamchiragchopra@gmail.com>
2018-10-26 20:34:31 +0530

    Add files via upload

---
+++ js/galery.js
@@ -0,0 +1,249 @@
+
+	$(function(){
+		var $gallery = $('.gallery a').simpleLightbox();
+		
+		$gallery.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		var $gallery2 = $('.gallery2 a').simpleLightbox();
+		
+		$gallery2.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 3
+		var $gallery3 = $('.gallery3 a').simpleLightbox();
+		
+		$gallery3.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 4
+		var $gallery4 = $('.gallery4 a').simpleLightbox();
+		
+		$gallery4.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 5
+		var $gallery5 = $('.gallery5 a').simpleLightbox();
+		
+		$gallery5.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 6
+		var $gallery6 = $('.gallery6 a').simpleLightbox();
+		
+		$gallery6.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 7
+		var $gallery7 = $('.gallery7 a').simpleLightbox();
+		
+		$gallery7.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 8
+		var $gallery8 = $('.gallery8 a').simpleLightbox();
+		
+		$gallery8.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 9
+		var $gallery9 = $('.gallery9 a').simpleLightbox();
+		
+		$gallery9.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 10
+		var $gallery10 = $('.gallery10 a').simpleLightbox();
+		
+		$gallery10.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 11
+		var $gallery11 = $('.gallery11 a').simpleLightbox();
+		
+		$gallery11.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 12
+		var $gallery12 = $('.gallery12 a').simpleLightbox();
+		
+		$gallery12.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 13
+		var $gallery13 = $('.gallery13 a').simpleLightbox();
+		
+		$gallery13.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 14
+		var $gallery14 = $('.gallery14 a').simpleLightbox();
+		
+		$gallery14.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 15
+		var $gallery15 = $('.gallery15 a').simpleLightbox();
+		
+		$gallery15.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 16
+		var $gallery16 = $('.gallery16 a').simpleLightbox();
+		
+		$gallery16.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 17
+		var $gallery17 = $('.gallery17 a').simpleLightbox();
+		
+		$gallery17.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 18
+		var $gallery18 = $('.gallery18 a').simpleLightbox();
+		
+		$gallery18.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 19
+		var $gallery19 = $('.gallery19 a').simpleLightbox();
+		
+		$gallery19.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 20
+		var $gallery20 = $('.gallery20 a').simpleLightbox();
+		
+		$gallery20.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 21
+		var $gallery21 = $('.gallery21 a').simpleLightbox();
+		
+		$gallery21.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 22
+		var $gallery22 = $('.gallery22 a').simpleLightbox();
+		
+		$gallery22.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 23
+		var $gallery23 = $('.gallery23 a').simpleLightbox();
+		
+		$gallery23.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 24
+		var $gallery24 = $('.gallery24 a').simpleLightbox();
+		
+		$gallery24.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 25
+		var $gallery25 = $('.gallery25 a').simpleLightbox();
+		
+		$gallery25.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 26
+		var $gallery26 = $('.gallery26 a').simpleLightbox();
+		
+		$gallery26.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 27
+		var $gallery27 = $('.gallery27 a').simpleLightbox();
+		
+		$gallery27.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 28
+		var $gallery28 = $('.gallery28 a').simpleLightbox();
+		
+		$gallery28.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 29
+		var $gallery29 = $('.gallery29 a').simpleLightbox();
+		
+		$gallery29.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 30
+		var $gallery30 = $('.gallery30 a').simpleLightbox();
+		
+		$gallery30.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		// gallery 31
+		var $gallery31 = $('.gallery31 a').simpleLightbox();
+		
+		$gallery31.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		// gallery 32
+		var $gallery32 = $('.gallery32 a').simpleLightbox();
+		
+		$gallery32.on('show.simplelightbox', function(){
+			console.log('Requested for showing');
+		})
+		
+		.on('shown.simplelightbox', function(){
+			console.log('Shown');
+		})
+		.on('close.simplelightbox', function(){
+			console.log('Requested for closing');
+		})
+		.on('closed.simplelightbox', function(){
+			console.log('Closed');
+		})
+		.on('change.simplelightbox', function(){
+			console.log('Requested for change');
+		})
+		.on('next.simplelightbox', function(){
+			console.log('Requested for next');
+		})
+		.on('prev.simplelightbox', function(){
+			console.log('Requested for prev');
+		})
+		.on('nextImageLoaded.simplelightbox', function(){
+			console.log('Next image loaded');
+		})
+		.on('prevImageLoaded.simplelightbox', function(){
+			console.log('Prev image loaded');
+		})
+		.on('changed.simplelightbox', function(){
+			console.log('Image changed');
+		})
+		.on('nextDone.simplelightbox', function(){
+			console.log('Image changed to next');
+		})
+		.on('prevDone.simplelightbox', function(){
+			console.log('Image changed to prev');
+		})
+		.on('error.simplelightbox', function(e){
+			console.log('No image found, go to the next/prev');
+			console.log(e);
+		});
+	});
